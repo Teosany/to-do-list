a3a802ff779aa36239132c0331b5c591c9bee254
Teo <teosany@icloud.com>
2024-02-08 15:56:51 +0100

    for zeabur1

--- database.class.php
+++ database.class.php
@@ -6,10 +6,19 @@ class DataBase extends PDO {
     private string $_DB_NAME = 'todo';
 
     public function __construct(){
-        try {
-            parent::__construct("mysql:host=" . $this->_DB_HOST . ";dbname=" . $this->_DB_NAME, $this->_DB_USER, $this->_DB_PASS);
-        } catch (PDOException $e) {
-            echo "Erreur de connection : ".$e->getMessage();
+        if (empty($_ENV["DB_HOST"])) {
+            try {
+                parent::__construct("mysql:host=" . $this->_DB_HOST . ";dbname=" . $this->_DB_NAME, $this->_DB_USER, $this->_DB_PASS);
+            } catch (PDOException $e) {
+                echo "Erreur de connection : " . $e->getMessage();
+            }
+        }
+        if (!empty($_ENV["DB_HOST"])) {
+            try {
+                parent::__construct("mysql:host=" . $_ENV["DB_HOST"] . ";dbname=" . $_ENV["DB_NAME"], $_ENV["DB_USER"], $_ENV["DB_PASS"]);
+            } catch (PDOException $e) {
+                echo "Erreur de connection : " . $e->getMessage();
+            }
         }
     }
 }
